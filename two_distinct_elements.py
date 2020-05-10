29b75dfab3b0092b22ad9fd6c86855fc0d070c8d
sandydesu@gmail.com <sandydesu@gmail.com>
2020-05-10 11:00:56 +0100

    added two_distinct_elements.py

---
+++ two_distinct_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Sandy Lee"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
