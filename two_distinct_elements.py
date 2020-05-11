8f9b2d54f2adf116a82df4be3c4f1d5369c85b46
sandydesu@gmail.com <sandydesu@gmail.com>
2020-05-11 17:16:17 +0100

    added complete two_distint_elements.py

--- two_distinct_elements.py
+++ two_distinct_elements.py
@@ -8,8 +8,20 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 
+def return_uniques(list):
+    uniques = []
+    for number in list:
+        if list.count(number) == 1:
+            uniques.append(number)
+    return uniques
+
+
 def main():
-    pass
+    print(return_uniques([1, 9, 8, 8, 7, 6, 1, 6]))
+
+    print(return_uniques([5, 5, 2, 4, 4, 4, 9, 9, 9, 1]))
+
+    print(return_uniques([9, 5, 6, 8, 7, 7, 1, 1, 1, 1, 1, 9, 8]))
 
 
 if __name__ == "__main__":
