aef7f4ad4cd1c3c5b3766eb90881abcadb3ef22b
sandydesu@gmail.com <sandydesu@gmail.com>
2020-05-12 10:45:43 +0100

    karaca_encrpytion_algorithm - work in progress

---
+++ karaca_encryption_algorithm.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Sandy Lee"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import sys
+
+
+def encrypt(message=None):
+    cipher = [message.lower()]
+    step_1 = reversed(cipher)
+    return step_1
+
+
+def main():
+    text = encrypt("test")
+    print(list(text))
+    # while True:
+    #     text = input('Enter word to encrypt:')
+    #     print(list(encrypt(text)))
+    #
+    #     while True:
+    #         again = input('Again? y/n')
+    #         if again in ('y', 'n'):
+    #             break
+    #         else:
+    #             print('Invalid Input')
+    #     if again == 'y':
+    #         continue
+    #     sys.exit('Goodbye')
+
+
+if __name__ == "__main__":
+    main()
