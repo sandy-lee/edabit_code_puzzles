dd4c757fa0c43b3779d4fc6b4fc4510944426054
sandydesu@gmail.com <sandydesu@gmail.com>
2020-05-13 18:50:27 +0100

    karaca_encryption_algorithm.py complete

--- karaca_encryption_algorithm.py
+++ karaca_encryption_algorithm.py
@@ -11,29 +11,43 @@ import sys
 
 
 def encrypt(message=None):
-    cipher = [message.lower()]
-    step_1 = reversed(cipher)
-    return step_1
+    """
+    :param message:
+    :return:
+    """
+    response = ""
+    vowel_mapping = {'a': 'O',
+                     'e': '1',
+                     'o': '2',
+                     'u': '3'}
+    final_append = ['a', 'c', 'a']
+
+    lower_case = message.lower()
+    reverse_order = list(reversed(lower_case))
+    vowel_replace = reverse_order.copy()
+    for index, letter in enumerate(vowel_replace):
+        if letter in vowel_mapping.keys():
+            vowel_replace[index] = vowel_mapping[letter]
+    append_step = vowel_replace + final_append
+    return response.join(append_step)
 
 
 def main():
-    text = encrypt("test")
-    print(list(text))
+    """
+    lorum ipsum
+    """
+    while True:
+        text = input('Enter word to encrypt:')
+        print(encrypt(text))
 
-    # TODO: how to make it print out a reverse list as a sting from a generator
-    # while True:
-    #     text = input('Enter word to encrypt:')
-    #     print(list(encrypt(text)))
-    #
-    #     while True:
-    #         again = input('Again? y/n')
-    #         if again in ('y', 'n'):
-    #             break
-    #         else:
-    #             print('Invalid Input')
-    #     if again == 'y':
-    #         continue
-    #     sys.exit('Goodbye')
+        while True:
+            again = input('Again? y/n')
+            if again in ('y', 'n'):
+                break
+            print('Invalid Input')
+        if again == 'y':
+            continue
+        sys.exit('Goodbye')
 
 
 if __name__ == "__main__":
