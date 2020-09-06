34caf75583c98dcd6411374ba3edd6735e3976a7
sandydesu@gmail.com <sandydesu@gmail.com>
2020-09-06 21:49:32 +0100

    added kempner_function

---
+++ src/kempner_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Sandy Lee"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def function_one:
+    pass
+
+
+def main():
+  pass
+
+
+if __name__ == "__main__":
+    main()
