fcbc0155ea3ce0f779f5026c1fa83bfed2400cbc
sandydesu@gmail.com <sandydesu@gmail.com>
2020-09-11 09:02:24 +0100

    division by zero error

--- src/kempner_function.py
+++ src/kempner_function.py
@@ -7,31 +7,26 @@ __author__ = "Sandy Lee"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+import math 
 
-def factorial(n):
-    if n == 1:
-        return n
-    else:
-        return n*factorial(n-1)
+# def factorial(n):
+#     if n == 1:
+#         return n
+#     else:
+#         return n*factorial(n-1)
 
 
 def kempner(n):
-    if factorial(n) / n == 0:
+    if math.factorial(n) / n == 0:
+        print(math.factorial(n))
+        print(n)
         return n
     else:
         return kempner(n-1)
 
 
-# def kempner(n):
-#     divisor = n
-#     if factorial(n) / divisor == 0:
-#         return ("it works!!")
-#     else:
-#         return("it sucks")
-
-
 def main():
-    print(kempner(6))
+    print(kempner(4))
 
 
 if __name__ == "__main__":
