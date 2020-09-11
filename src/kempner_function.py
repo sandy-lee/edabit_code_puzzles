e9329df8b964dbf8e5384938dd664ac477903acf
sandydesu@gmail.com <sandydesu@gmail.com>
2020-09-11 05:25:27 +0100

    added recursion and factors

--- src/kempner_function.py
+++ src/kempner_function.py
@@ -8,12 +8,26 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 
-def function_one:
-    pass
+def factorial(n):
+    number = 0
+    for i, n in enumerate(n):
+        number = i*n
+    return number
+
+
+
+def kempner(n):
+  if (n > 0):
+    result = n + kempner(n - 1)
+    print(n)
+  else:
+    result = 0
+  return result
+  
 
 
 def main():
-  pass
+  factorial(5)
 
 
 if __name__ == "__main__":
