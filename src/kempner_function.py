b4291cb01cf0aba600ccf4a9d5c7cae9528cf69d
sandydesu@gmail.com <sandydesu@gmail.com>
2020-09-14 11:47:32 +0100

    added solutions

--- src/kempner_function.py
+++ src/kempner_function.py
@@ -7,7 +7,9 @@ __author__ = "Sandy Lee"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-import math
+# import math
+
+from math import factorial as f
 
 divisor = 0
 # def factorial(n):
@@ -30,20 +32,30 @@ divisor = 0
 #     elif math.factorial(n) % divisor > 0:
 #         return kempner(n-1)
 
-def kempner(n):
-    for i in range(1, n):
-        if math.factorial(n) % divisor == 0:
-            return i
-        elif math.factorial(n) % divisor > 0:
-            return kempner(n-1)
+def kempner(n, i=1, total=1):
+    # return max(1, i-1) if total%n == 0 else kempner(n, i+1, total*i)
+    if total % n == 0:
+        return max(1, i-1)
+    else:
+        return kempner(n, i+1, total*i)
 
+#why does this wind down after the condition is met and go down again?
+
+
+# def kempner(n, i=1):
+#     # return kempner(n, i +1) if f(i)%n else i
+#     if f(i) % n:
+#         return kempner(n, i+1)
+#     else:
+#         return i
 
 
 def main():
     global divisor
-    number_to_check = 6
-    divisor = 6
+    number_to_check = 10
+    divisor = 10
     print(kempner(number_to_check))
+
 
 
 if __name__ == "__main__":
